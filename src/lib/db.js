040876aa2b18c7d0f5f7adba14643579504cf93a
vedanttyagi42-del <vedanttyagi42@gmail.com>
2025-12-18 15:51:40 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,5 @@
 import {Pool} from "pg"
 const db = new Pool({
-  connectionString: "postgresql://postgres:vOkZqJMnEeLIyPBsdjUBWnuwEXhhDDQZ@crossover.proxy.rlwy.net:59049/railway",
+  connectionString: process.env.DB_URL,
 })
 export default db
